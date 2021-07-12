268f793c2d578c54947f56b48048a19066a8e78a
IrinaSemGB <irina.semyachkina@mail.ru>
2021-07-12 15:00:46 +0300

    updated app with Boss level Challenge 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatefulWidget {
+
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+
+  int ballNumber = 1;
+
+  void getNewBallValue() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print('I got clicked - $ballNumber');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+          child: TextButton(
+            onPressed: (){
+              getNewBallValue();
+            },
+              child: Image.asset('images/ball$ballNumber.png')
+          ),
+        ),
+      ),
+    );
+  }
+}
+
